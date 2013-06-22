7446f4ed9407fae72a5770ccb2e467767bdbda30
Keyvan Fatehi <keyvanfatehi@gmail.com>
2013-06-22 03:27:28 -0700

    fix plugin

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -20,13 +20,7 @@ class YouTube < Liquid::Tag
   end
  
   def render(context)
-    <<-HTML
-    <iframe width="#{@width}" 
-            height="#{@height}" 
-            src="http://www.youtube.com/embed/#{@id}"
-            frameborder="0" allowfullscreen>
-    </iframe>
-    HTML
+    "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}\"></iframe>"
   end
  
   Liquid::Template.register_tag "youtube", self
